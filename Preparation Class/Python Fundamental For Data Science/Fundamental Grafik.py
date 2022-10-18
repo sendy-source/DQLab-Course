9add7a6c95492a12fe39855ab2c984335f1d1cfd
sendy <sendyanggriawan88@gmail.com>
2022-10-18 22:13:32 +0700

    update

---
+++ Preparation Class/Python Fundamental For Data Science/Fundamental Grafik.py
@@ -0,0 +1,44 @@
+#Bar Chart
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
+table.head()
+x_label = table['NAMA KELURAHAN']
+plt.bar(x=np.arange(len(x_label)),height=table['LAKI-LAKI WNI'])
+plt.show()
+
+
+#[Parameter dalam Grafik (Memberikan Nilai Axis dari data CSV)]
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
+table.head()
+
+x_label = table['NAMA KELURAHAN']
+plt.bar(x=np.arange(len(x_label)),height=table['LAKI-LAKI WNI'])
+
+plt.xticks(np.arange(len(x_label)), table['NAMA KELURAHAN'], rotation=30)
+plt.show()
+
+
+#[Menambahkan Title pada Grafik]
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
+table.head()
+
+x_label = table['NAMA KELURAHAN']
+plt.bar(x=np.arange(len(x_label)),height=table['LAKI-LAKI WNI'])
+plt.xticks(np.arange(len(x_label)), table['NAMA KELURAHAN'], rotation=90)
+plt.xlabel('Kelurahan di Jakarta Pusat')
+plt.ylabel('Jumlah Penduduk Laki - Laki')
+plt.title('Persebaran Jumlah Penduduk Laki- Laki di Jakarta Pusat')
+
+plt.show()
